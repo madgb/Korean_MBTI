b9e78500c90499746d0d5a7eb1ff67263192c91b
madgb <madgb00@gmail.com>
2020-06-29 00:22:17 -0400

    Flask Backend and MongoDB Atlas Database

---
+++ venv/app/static/js/index.js
@@ -0,0 +1,113 @@
+const result = [[], [], [], []];
+let count = 0;
+const btn = document.querySelectorAll("button");
+const btnArr = [...btn];
+btnArr.forEach((eachBtn) => {
+    eachBtn.addEventListener("click", (event) => {
+        const name = event.target.name;
+        const testType = event.target.parentElement.parentElement.classList[0];
+        if (testType === "jjjb") {
+            result[0].push(name);
+        }
+        if (testType === "jmbm") {
+            result[1].push(name);
+        }
+        if (testType === "mnbn") {
+            result[2].push(name);
+        }
+        if (testType === "mdsd") {
+            result[3].push(name);
+        }
+
+        if (count > 10) {
+            selectAge(result);
+        } else {
+            showHide(count);
+        }
+        count++;
+    });
+});
+
+const calcResult = function (arr, age) {
+    let finalResult = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i][0] === arr[i][1] || arr[i][0] === arr[i][2]) {
+            finalResult.push(arr[i][0]);
+        } else {
+            finalResult.push(arr[i][1]);
+        }
+    }
+    const agePart = document.querySelector('.age');
+    agePart.classList.add('hide');
+
+    const resultPart = document.querySelector('.result');
+    resultPart.classList.remove('hide');
+
+    const resultMBTIPart = document.querySelector('.result-mbti');
+    resultMBTIPart.textContent = finalResult.join('');
+
+    sendData(finalResult, age);
+};
+
+const showHide = function(count) {
+    const questions = document.querySelectorAll('.question');
+    const qAA = [...questions];
+    qAA[count].classList.add('hide');
+    qAA[count+1].classList.remove('hide');
+    qAA[count+1].classList.add('show');
+}
+
+const startButton = document.querySelector('.test-start-button');
+startButton.addEventListener("click", () => {
+    const introPart = document.querySelector('.intro');
+    introPart.classList.add('hide');
+    const testPart = document.querySelector(".test");
+    testPart.classList.remove('hide');
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+const translateForm = {
+    "짜": "A",
+    "짬": "B",
+    "부": "C",
+    "찍": "D",
+    "물": "E",
+    "비": "F",
+    "밀": "G",
+    "쌀": "H",
+};
+
+const selectAge = function(result) {
+    const testPart = document.querySelector('.test');
+    testPart.classList.add('hide');
+    const agePart = document.querySelector('.age');
+    agePart.classList.remove('hide');
+
+    const selectElement = document.querySelector('.age-select');
+    
+    selectElement.addEventListener('change', (event) => {
+        const age = event.target.value;
+        calcResult(result, age);
+    });
+}
+
+const sendData = function(finalResult, age) {
+    let mbti = '';
+    finalResult.forEach(letter => {
+        mbti += translateForm[letter];
+    });
+
+    const data = {mbti, age};
+    console.log('kmbti result - ready to fetch', data);
+
+    fetch('/my-result', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+}
---
+++ venv/app/static/js/result.js
@@ -0,0 +1,116 @@
+console.log("data", data);
+const allTypeChartElem = document.getElementById('allTypeChart');
+
+const teenTypeChartElem = document.getElementById('teenTypeChart');
+const twentyTypeChartElem = document.getElementById('twentyTypeChart');
+const thirtyTypeChartElem = document.getElementById('thirtyTypeChart');
+const FourtyTypeChartElem = document.getElementById('FourtyTypeChart');
+
+const teenEachChartElem = document.getElementById('teenEachChart');
+const twentyEachChartElem = document.getElementById('twentyEachChart');
+const thirtyEachChartElem = document.getElementById('thirtyEachChart');
+const fourtyEachChartElem = document.getElementById('fourtyEachChart');
+
+const translateForm = {
+    A: "짜장",
+    B: "짬뽕",
+    C: "부먹",
+    D: "찍먹",
+    E: "물냉",
+    F: "비냉",
+    G: "밀떡",
+    H: "쌀떡",
+};
+
+// MBTI 총 통계
+const totalType = data.totalType;
+
+// 나이대별 MBTI 통계
+const ageType = data.ageType;
+
+// 나이대별 선호 음식 통계
+const ageEachType = data.ageEachType;
+
+// 그래프 데이터용
+let totalTypeKr, ageEachTypeKr
+
+function distribute(dataType, className, each, label, elem) {
+    const wrapper = document.querySelector(className);
+    const typeVariable = Object.keys(dataType);
+    const typeValues = Object.values(dataType);
+    const allResult = letterToKorean(typeVariable, typeValues, each, label, elem);
+    wrapper.innerHTML = allResult;
+}
+
+function letterToKorean(typeVariable, typeValues, each, label, elem) {
+    let mappedType;
+    if (!each) {
+        mappedType = typeVariable.map((type) =>
+            type
+                .split("")
+                .map((letter) => translateForm[letter][0])
+                .join("")
+        );
+    } else {
+        mappedType = typeVariable.map((type) => translateForm[type]);
+    }
+
+    let allResult = "";
+    typeValues.forEach((eachVal, idx) => {
+        allResult += `<div>${mappedType[idx]}</div><div>${eachVal}</div>`;
+    });
+    createChart(elem, mappedType, typeValues, label);
+    return allResult;
+}
+
+distribute(totalType, ".total-type-wrapper", false, '전체 통계', allTypeChartElem);
+distribute(ageType.teen, ".age-type-wrapper .teen", false, '10대 통계', teenTypeChartElem);
+distribute(ageType.twenty, ".age-type-wrapper .twenty", false, '20대 통계', twentyTypeChartElem);
+distribute(ageType.thirty, ".age-type-wrapper .thirty", false, '30대 통계', thirtyTypeChartElem);
+distribute(ageType.fourty, ".age-type-wrapper .fourty", false, '40대 통계', FourtyTypeChartElem);
+distribute(ageEachType.teen, ".age-each-type-wrapper .teen", true, '10대 개별 통계', teenEachChartElem);
+distribute(ageEachType.twenty, ".age-each-type-wrapper .twenty", true, '20대 개별 통계', twentyEachChartElem);
+distribute(ageEachType.thirty, ".age-each-type-wrapper .thirty", true, '30대 개별 통계', thirtyEachChartElem);
+distribute(ageEachType.fourty, ".age-each-type-wrapper .fourty", true, '40대 개별 통계', fourtyEachChartElem);
+
+
+
+
+function createChart(elem, labels, data, label) {
+    new Chart(elem, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: label,
+                data: data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
